48f37d28f3e78758f88bef6309803a4400669161
mafarthi <maxfarthing1@gmail.com>
2024-06-18 14:31:00 -0400

    first fullstack app set up

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+    const [backEndData, setBackEndData] = useState([{}])
+
+    useEffect(() => {
+        fetch("/api")
+            .then(
+                response => response.json()
+            )
+            .then(data => {
+                setBackEndData(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div>
+            {typeof backEndData.users === 'undefined' ? (
+                <p>Loading...</p>
+            ) : (
+                backEndData.users.map((user, i) => ( 
+                    <p key={i}>{user}</p>
+                ))
+            )}
+        </div>
+    )
+}
+
+export default App
